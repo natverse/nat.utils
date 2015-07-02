14c56a777c63ea2e6102d0cc71bb41d2add8d820
Gregory Jefferis <jefferis@gmail.com>
2015-07-02 11:28:14 +0100

    document, examples, export

--- R/fsutils.r
+++ R/fsutils.r
@@ -64,6 +64,26 @@ abs2rel<-function(path,stempath=getwd(),StopIfNoCommonPath=FALSE){
   relpath
 }
 
+#' Find common prefix of two or more (normalised) file paths
+#' 
+#' @param paths Character vector of file paths
+#' @param normalise Whether to normalise \code{paths} (with 
+#'   \code{\link{normalizePath}})
+#' @param fsep Optional path separator (defaults to \code{.Platform$file.sep})
+#' @return Character vector of common prefix, \code{""} when there is no common
+#'   prefix, or the original value of \code{paths} when fewer than 2 paths were
+#'   supplied.
+#' @export
+#' @examples
+#' common_path(c("/a","/b"))
+#' common_path(c("/a/b/","/a/b"))
+#' common_path(c("/a/b/d","/a/b/c/d"))
+#' common_path(c("/a/b/d","/b/c/d"))
+#' common_path(c("a","b"))
+#' common_path(c("","/a"))
+#' common_path(c("~","~/"))
+#' common_path(c("~/a/b/d","~/a/b/c/d"), normalise = FALSE)
+#' common_path(c("~","~/"), normalise = FALSE)
 common_path<-function(paths, normalise=TRUE, fsep=.Platform$file.sep) {
   if(normalise)
     paths=normalizePath(paths, mustWork = F)
