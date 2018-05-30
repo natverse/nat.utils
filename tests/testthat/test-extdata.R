982f1b5e162fcdec9aaba3b6149722829bc30916
Gregory Jefferis <jefferis@gmail.com>
2018-05-30 14:32:29 +0100

    test error - not sure how I missed this

--- tests/testthat/test-extdata.R
+++ tests/testthat/test-extdata.R
@@ -1,6 +1,6 @@
 context("test-extdata.R")
 
 test_that("can make a neuronlist from parts", {
-  delayedAssign('wurgle', make_nl_from_parts('sample.rds', package='nat.utils'))
+  delayedAssign('wurgle', read_nl_from_parts('sample.rds', package='nat.utils'))
   expect_is(wurgle, 'neuronlist')
 })
